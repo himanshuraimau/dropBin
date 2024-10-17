67278d2f297652104fe3e2b4fcd30522d28ea99c
Himanshu Rai <himanshuraimau9@gmail.com>
2024-10-18 00:53:29 +0530

    fix folder structure

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import Body from "./components/HomePage/Body";
 import Header from "./components/HomePage/Header";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-amber-50 text-foreground">
       <Header />
+      <Body/>
     </div>
   )
 }
